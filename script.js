6b5ee31b7e1be183f68694086acc3e45860ad590
Aary Angre <87111984+YoBoii07@users.noreply.github.com>
2021-09-02 19:56:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,47 @@
-/* Oui Oui this is still under development*/
+/* I could've used jquery but i want to brush up my js skills
+so I decided to write the zoomin and zoomout code*/
+var width=16;
+var difference=2;
+var interveralID =0;
+function zoomin()
+		{
+			clearInterval(interveralID);
+			interveralID=setInterval(expand,10);
+      //this times the function and expands object after every 10ms
+		}
+function zoomout()
+		{
+			clearInterval(interveralID);
+			interveralID=setInterval(shrink,10);
+      //this shrinks the object after every 10ms
+		}
+
+//now we have both the functions defined
+function expand()
+		{
+			if(width<20)
+			{
+				width = width+difference;
+				document.getElementById("img1").style.width=width;
+				console.log(width);
+			}
+			else
+			{
+				clearInterval(interveralID);
+			}
+			
+		}
+function shrink()
+		{
+			if(width>16)
+			{
+				width = width-difference;
+				document.getElementById("img1").style.width=width;
+				console.log(width);
+			}
+			else
+			{
+				clearInterval(interveralID);
+			}
+			
+		}
